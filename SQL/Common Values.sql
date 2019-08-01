3e34cd6229046a9a94bbdae8f2d488b205d0158f
Michael Pace <Michael.Pace@bjss.com>
2019-08-01 12:16:16 +0100

    initial database script files

---
+++ SQL/Common Values.sql
@@ -0,0 +1,13 @@
+USE buyingcatalogue;
+GO
+
+INSERT INTO [dbo].[StandardCategory] (Id, CategoryName) VALUES (1, 'Overarching');
+INSERT INTO [dbo].[StandardCategory] (Id, CategoryName) VALUES (2, 'Interoperability');
+INSERT INTO [dbo].[StandardCategory] (Id, CategoryName) VALUES (3, 'Context');
+INSERT INTO [dbo].[StandardCategory] (Id, CategoryName) VALUES (4, 'Capability');
+GO
+
+INSERT INTO [dbo].[StandardStatus] (Id, StatusName) VALUES (1, 'Effective');
+GO
+INSERT INTO [dbo].[CapabilityStatus] (Id, StatusName) VALUES (1, 'Effective');
+GO
