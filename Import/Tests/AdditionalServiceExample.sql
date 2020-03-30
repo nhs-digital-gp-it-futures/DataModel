29778f979bdc3efa23ed188ab2f5a400638cc375
michaelpace-bjss <michael.pace@bjss.com>
2020-03-30 11:15:58 +0100

    task5577 amend for remiew comments

--- Import/Tests/AdditionalServiceExample.sql
+++ Import/Tests/AdditionalServiceExample.sql
@@ -5,7 +5,7 @@ Declarations
 
 DECLARE @SupplierId varchar(6)
 DECLARE @ParentSolutionId varchar(16)
-DECLARE @AdditionServiceId varchar(14)
+DECLARE @AdditionalServiceId varchar(14)
 DECLARE @ServiceName varchar(255)
 DECLARE @ServiceSummary varchar(300)
 DECLARE @ServiceDescription varchar(3000)
@@ -15,13 +15,13 @@ DECLARE @Capabilities AdditionalServiceImportCapability
 SET DATA HERE
 ***/
 
-SET @SupplierId = '10000'
-SET @ParentSolutionId = '10000-001'
-SET @AdditionServiceId = '10000-001A001'
+SET @SupplierId = '99999'
+SET @ParentSolutionId = '99999-89'
+SET @AdditionalServiceId = '99999-89A001'
 SET @ServiceName = 'Not Video Consultation'
-SET @ServiceSummary = 'A Not Video Consultation service for EMIS Web GP' -- Can be set to NULL
-SET @ServiceDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-INSERT INTO @Capabilities VALUES('C20'), ('C6')
+SET @ServiceSummary = 'A Not Video Consultation service for NotEMIS Web GP' -- Can be set to NULL
+SET @ServiceDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' -- Can be set to NULL
+INSERT INTO @Capabilities VALUES('C6')
 
 /***
 Echo data set
@@ -29,7 +29,7 @@ Echo data set
 
 print @SupplierId 
 print @ParentSolutionId 
-print @AdditionServiceId 
+print @AdditionalServiceId 
 print @ServiceName 
 print @ServiceSummary 
 print @ServiceDescription
@@ -39,7 +39,7 @@ select * from @Capabilities
 EXEC AddtionServiceImport
 	@SupplierId,
 	@ParentSolutionId,
-	@AdditionServiceId,
+	@AdditionalServiceId,
 	@ServiceName,
 	@ServiceSummary,
 	@ServiceDescription,
